ed013583f83503bc49533d8ecf31e23b7a3a4481
ThaMad <thanudi.m@gmail.com>
2023-05-04 04:19:35 +0200

    Design Launch, Home, Signup, Sign in

---
+++ Goldie/Goldie/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Goldie
+//
+//  Created by Thanudi Madawala on 04/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
