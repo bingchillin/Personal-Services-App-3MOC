2d3ae859ffd4e8b33cbcc22a62f3e7b859f3983b
bingchillin <92374981+bingchillin@users.noreply.github.com>
2023-07-16 03:27:05 +0200

    Cleaned details display

--- Flutter/lib/pages/details_user.dart
+++ Flutter/lib/pages/details_user.dart
@@ -1,26 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:goldie_studio/webservices/user_class.dart';
 
-class UserDetailsWidget extends StatelessWidget {
+class UserDetailsWidget extends StatefulWidget {
   final User user;
 
   const UserDetailsWidget({Key? key, required this.user}) : super(key: key);
 
   @override
+  _UserDetailsWidgetState createState() => _UserDetailsWidgetState();
+}
+
+class _UserDetailsWidgetState extends State<UserDetailsWidget> {
+  late List<Map<String, String>> _fieldList;
+  late List<TextEditingController> _textControllers;
+
+  @override
+  void initState() {
+    super.initState();
+    _fieldList = [
+      {'Prénom': widget.user.firstname ?? ''},
+      {'Nom': widget.user.lastname ?? ''},
+      {'Email': widget.user.email ?? ''},
+      {'Date de naissance': widget.user.birthdate ?? ''},
+      {'Validé': widget.user.validated?.toString() ?? ''},
+      {'Note': widget.user.note?.toString() ?? ''},
+      {'Profession': widget.user.profession?.toString() ?? ''},
+      {'Rôle': widget.user.role?.toString() ?? ''},
+    ];
+
+    _textControllers = _fieldList
+        .map((field) => TextEditingController(text: field.values.first))
+        .toList();
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _textControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Détails de ${user.firstname}'),
+        title: Text('Détails de ${widget.user.firstname}'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('ID: ${user.id}'),
-            Text('Prénom: ${user.firstname}'),
-            Text('Profession: ${user.profession}'),
-            Text('Rôle: ${user.role}'),
-            Text('Validé: ${user.validated}'),
+            for (var i = 0; i < _fieldList.length; i++)
+              TextField(
+                controller: _textControllers[i],
+                decoration: InputDecoration(labelText: _fieldList[i].keys.first),
+              ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Ajouter la logique de mise à jour des informations de l'utilisateur
+              },
+              child: const Text('Enregistrer'),
+            ),
           ],
         ),
       ),
