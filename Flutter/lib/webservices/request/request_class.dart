87771fcad8617dacab263ebc9d7ed86fbc5974a7
Pokoki <92927093+Pokoki@users.noreply.github.com>
2023-07-17 18:45:02 +0200

    create class for request

--- Flutter/lib/webservices/request/request_class.dart
+++ Flutter/lib/webservices/request/request_class.dart
@@ -6,4 +6,48 @@ class Request{
   final int? type;
   final int? slots;
   final bool? accepted;
+  final bool? done;
+
+  Request({
+    this.id,
+    required this.title,
+    required this.timer,
+    required this.client,
+    required this.type,
+    required this.slots,
+    this.accepted,
+    this.done,
+  });
+
+  factory Request.fromJson(Map<String, dynamic> json) {
+    return Request(
+      id: json['id'] is int ? json['id'] : null,
+      title: json['title'] is String ? json['title'] : null,
+      timer: json['timer'] is String ? json['timer'] : null,
+      client: json['client'] is int ? json['client'] : null,
+      type: json['type'] is int ? json['type'] : null,
+      slots: json['slots'] is int ? json['slots'] : null,
+      accepted: json['accepted'] is bool ? json['accepted'] : null,
+      done: json['done'] is bool ? json['done'] : null,
+    );
+  }
+
+  // tojson
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'timer': timer,
+      'client': client,
+      'type': type,
+      'slots': slots,
+      'accepted': accepted,
+      'done': done,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Request{id: $id, title: $title, timer: $timer, client: $client, type: $type, slots: $slots, accepted: $accepted, done: $done}';
+  }
 }
