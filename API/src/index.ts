2ccd9a0726a4f1b2c589d5dfcd2ed5d965310912
Pokoki <92927093+Pokoki@users.noreply.github.com>
2023-07-17 18:16:21 +0200

    core update

--- API/src/index.ts
+++ API/src/index.ts
@@ -6,6 +6,7 @@ import requetesController from './requetes/Controller';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import bodyParser from 'body-parser';
+const cors = require('cors');
 
 //Server
 const app = express();
@@ -33,6 +34,9 @@ const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*'
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
