fa91bd30701b10f28c9f96465a2ac8da82b0d0e8
Pokoki <92927093+Pokoki@users.noreply.github.com>
2023-07-18 03:16:07 +0200

    login screen

---
+++ Android/app/src/main/java/com/example/goldie/ApiConnectorFragment.kt
@@ -0,0 +1,20 @@
+package com.example.goldie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ApiConnectorFragment : Fragment() {
+
+    // Ajouter les vues et la logique pour afficher l'interface utilisateur du connecteur API ici
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_api_connector, container, false)
+
+        // Initialiser les vues et configurer la logique de l'interface utilisateur pour le fragment du connecteur API
+
+        return view
+    }
+}
--- Android/app/src/main/java/com/example/goldie/MainActivity.kt
+++ Android/app/src/main/java/com/example/goldie/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.goldie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginFragment: LoginFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialiser le fragment de connexion
+        loginFragment = LoginFragment()
+
+        // Afficher le fragment de connexion dans le conteneur de fragments
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, loginFragment)
+            .commit()
+    }
+
+    fun login(email: String, password: String) {
+        // Implémenter la logique de connexion ici, par exemple, appeler l'API
+
+        // Après une connexion réussie, basculer vers le fragment du connecteur API
+        val apiConnectorFragment = ApiConnectorFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, apiConnectorFragment)
+            .addToBackStack(null) // Cela permet la navigation arrière
+            .commit()
     }
 }
