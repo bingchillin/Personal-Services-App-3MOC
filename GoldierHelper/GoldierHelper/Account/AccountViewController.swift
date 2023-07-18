ae2c94766eec4e4ebfc75978412611b5aec20bee
ThaMad <thanudi.m@gmail.com>
2023-07-18 05:38:04 +0200

    [WIP] Login with API

---
+++ GoldierHelper/GoldierHelper/Account/AccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AccountViewController.swift
+//  GoldierHelper
+//
+//  Created by Thanudi Madawala on 18/07/2023.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    class func newInstance() -> AccountViewController {
+        let accountViewController = AccountViewController()
+        
+        return accountViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GoldierHelper/GoldierHelper/AllRequest/AllRequestViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AllRequestViewController.swift
+//  GoldierHelper
+//
+//  Created by Thanudi Madawala on 18/07/2023.
+//
+
+import UIKit
+
+class AllRequestViewController: UIViewController {
+    
+    
+    class func newInstance() -> AllRequestViewController {
+        let allRequestViewController = AllRequestViewController()
+        
+        return allRequestViewController
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
