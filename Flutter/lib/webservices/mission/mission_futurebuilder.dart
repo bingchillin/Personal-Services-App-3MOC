cb3839b575b2524a2fa8e9cfc334d11c31858dc5
bingchillin <92374981+bingchillin@users.noreply.github.com>
2023-07-31 23:35:45 +0200

    Fixed missions tab, list of static values

--- Flutter/lib/webservices/mission/mission_futurebuilder.dart
+++ Flutter/lib/webservices/mission/mission_futurebuilder.dart
@@ -12,7 +12,6 @@ class MissionsWidget extends StatelessWidget {
   }
 }
 
-
 class MissionFutureBuilder extends StatefulWidget {
   const MissionFutureBuilder({super.key});
 
@@ -21,14 +20,47 @@ class MissionFutureBuilder extends StatefulWidget {
 }
 
 class _MissionFutureBuilderState extends State<MissionFutureBuilder> {
+  List<Mission> _missions = [];
+  List<Mission> _filteredMissions = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMissions();
+  }
+
+  void _loadMissions() {
+    // Ici, vous pouvez charger vos missions statiques ou supprimer complètement cette méthode
+    // et initialiser _missions directement avec vos données statiques
+    _missions = [
+      Mission(id: 1, title: 'Mission 1', description: 'Description mission 1', date: '2023-07-15'),
+      Mission(id: 2, title: 'Mission 2', description: 'Description mission 2', date: '2023-07-16'),
+      Mission(id: 3, title: 'Mission 3', description: 'Description mission 3', date: '2023-07-17'),
+      // Ajoutez vos autres missions statiques ici
+    ];
+    _filteredMissions = _missions;
+  }
+
+  void _updateFilteredMissions(String searchText) {
+    setState(() {
+      _filteredMissions = _missions
+          .where((mission) =>
+          mission.title.toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          const TextField(
-            decoration: InputDecoration(
+          TextField(
+            controller: _searchController,
+            onChanged: _updateFilteredMissions,
+            decoration: const InputDecoration(
               labelText: 'Rechercher une mission',
             ),
           ),
@@ -36,41 +68,74 @@ class _MissionFutureBuilderState extends State<MissionFutureBuilder> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  _loadMissions();
+                },
                 icon: const Icon(Icons.refresh),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  // Code pour ajouter une nouvelle mission
+                },
                 icon: const Icon(Icons.add),
               ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return const ListTile(
-                      title: Row(
-                        children: [
-                          Expanded(child: TitleWidget(title: 'ID')),
-                          Spacer(),
-                          Expanded(child: TitleWidget(title: 'Prénom')),
-                          Spacer(),
-                          Expanded(child: TitleWidget(title: 'Profession')),
-                          Spacer(),
-                          Expanded(child: TitleWidget(title: 'Rôle')),
-                          Spacer(),
-                          Expanded(child: TitleWidget(title: 'Validé')),
-                          Spacer(),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
             ],
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredMissions.length,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return const ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(child: TitleWidget(title: 'ID')),
+                        Spacer(),
+                        Expanded(child: TitleWidget(title: 'Titre')),
+                        Spacer(),
+                        Expanded(child: TitleWidget(title: 'Description')),
+                        Spacer(),
+                        Expanded(child: TitleWidget(title: 'Date')),
+                      ],
+                    ),
+                  );
+                }
+                final mission = _filteredMissions[index - 1];
+                return Card(
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(child: Text('${mission.id}')),
+                        const Spacer(),
+                        Expanded(child: Text(mission.title)),
+                        const Spacer(),
+                        Expanded(child: Text(mission.description)),
+                        const Spacer(),
+                        Expanded(child: Text(mission.date)),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+class Mission {
+  final int id;
+  final String title;
+  final String description;
+  final String date;
+
+  Mission({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.date,
+  });
 }
