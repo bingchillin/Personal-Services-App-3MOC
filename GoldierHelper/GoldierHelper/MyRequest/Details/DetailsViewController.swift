def6e45ede3cc6bfc98c39901667b2905795e366
bingchillin <92374981+bingchillin@users.noreply.github.com>
2023-08-29 23:31:31 +0200

    Fixed buttons

--- GoldierHelper/GoldierHelper/MyRequest/Details/DetailsViewController.swift
+++ GoldierHelper/GoldierHelper/MyRequest/Details/DetailsViewController.swift
@@ -12,7 +12,6 @@ class DetailsViewController: UIViewController {
         return controller
     }
     
-    
     let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "titre"
@@ -46,25 +45,23 @@ class DetailsViewController: UIViewController {
         return label
     }()
     
-
-        
-        let abandonButton: UIButton = {
-            let button = UIButton()
-            button.setTitle("Abandonner la mission", for: .normal)
-            button.backgroundColor = .red
-            button.addTarget(MyRequestViewController.self, action: #selector(abandonButtonTapped), for: .touchUpInside)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
-
-        let takeButton: UIButton = {
-            let button = UIButton()
-            button.setTitle("S'inscrire à la mission", for: .normal)
-            button.backgroundColor = .green
-            button.addTarget(MyRequestViewController.self, action: #selector(registerTaskButtonTapped), for: .touchUpInside)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
+    let abandonButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Abandonner la mission", for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(abandonButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let takeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("S'inscrire à la mission", for: .normal)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(registerTaskButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,57 +69,55 @@ class DetailsViewController: UIViewController {
         view.backgroundColor = .white
         self.titleLabel.text = "\(self.requete.title)"
         
-
-            view.addSubview(titleLabel)
-            view.addSubview(userInfoView)
-            userInfoView.addSubview(nameLabel)
-            userInfoView.addSubview(subjectLabel)
+        view.addSubview(titleLabel)
+        view.addSubview(userInfoView)
+        userInfoView.addSubview(nameLabel)
+        userInfoView.addSubview(subjectLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            
+            userInfoView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
+            userInfoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
+            userInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
+            userInfoView.heightAnchor.constraint(equalToConstant: 250),
+            
+            nameLabel.topAnchor.constraint(equalTo: userInfoView.topAnchor, constant: 30),
+            nameLabel.leadingAnchor.constraint(equalTo: userInfoView.leadingAnchor, constant: 30),
+            
+            subjectLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            subjectLabel.leadingAnchor.constraint(equalTo: userInfoView.leadingAnchor, constant: 30),
+        ])
+        
+        // Ajoutez le bouton approprié en fonction de la valeur de isAll
+        if isAll {
+            // Ajoutez le bouton pour s'inscrire à la mission
+            view.addSubview(takeButton)
+            
+            NSLayoutConstraint.activate([
+                takeButton.topAnchor.constraint(equalTo: userInfoView.bottomAnchor, constant: 20),
+                takeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                takeButton.widthAnchor.constraint(equalToConstant: 200),
+                takeButton.heightAnchor.constraint(equalToConstant: 40)
+            ])
+        } else {
+            // Ajoutez le bouton pour abandonner la mission
             view.addSubview(abandonButton)
             
             NSLayoutConstraint.activate([
-                titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-                titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
-                
-                userInfoView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30),
-                userInfoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80),
-                userInfoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -80),
-                userInfoView.heightAnchor.constraint(equalToConstant: 250),
-                
-                nameLabel.topAnchor.constraint(equalTo: userInfoView.topAnchor, constant: 30),
-                nameLabel.leadingAnchor.constraint(equalTo: userInfoView.leadingAnchor, constant: 30),
-                
-                subjectLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
-                subjectLabel.leadingAnchor.constraint(equalTo: userInfoView.leadingAnchor, constant: 30),
-            ])
-                
-                if isAll == false {
-                    
-                    NSLayoutConstraint.activate([
-                        abandonButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                        abandonButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-                        abandonButton.widthAnchor.constraint(equalToConstant: 200),
-                        abandonButton.heightAnchor.constraint(equalToConstant: 40)
-                    ])
-                    
-                }
-        /*
-        else {
-            NSLayoutConstraint.activate([
-                takeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                takeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-                takeButton.widthAnchor.constraint(equalToConstant: 200),
-                takeButton.heightAnchor.constraint(equalToConstant: 40)
-                
+                abandonButton.topAnchor.constraint(equalTo: userInfoView.bottomAnchor, constant: 20),
+                abandonButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                abandonButton.widthAnchor.constraint(equalToConstant: 200),
+                abandonButton.heightAnchor.constraint(equalToConstant: 40)
             ])
         }
-         */
-        
-        
     }
     
     @objc func abandonButtonTapped() {
-        // Action when logout button is tapped
+        // Action when abandon button is tapped
     }
+    
     @objc func registerTaskButtonTapped() {
         // Action when register button is tapped
     }
